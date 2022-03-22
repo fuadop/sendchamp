2ab453a6bdb1cd949390d1d02ae9055a34978643
fuadop <fuadolatunji@gmail.com>
2022-03-22 10:14:16 +0100

    chore: test sms.Send function

---
+++ sms_test.go
@@ -0,0 +1,25 @@
+package sendchamp_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/fuadop/sendchamp"
+)
+
+func TestSendSms(t *testing.T) {
+	key, ok := os.LookupEnv("PUBLIC_KEY")
+	if !ok {
+		key = "PUBLIC_KEY"
+	}
+
+	client := sendchamp.NewClient(key, "live")
+	res, err := client.NewSms().Send("ASF", []string{"2348153207998"}, "TestSendSingleSms", sendchamp.RouteNonDND)
+	if err != nil {
+		t.Fail()
+	}
+
+	if res.Status != "success" {
+		t.Fail()
+	}
+}
