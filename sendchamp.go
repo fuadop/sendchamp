c2561e8aacc67d9aeb770b22c5bd2eaec56eaafd
fuadop <fuadolatunji@gmail.com>
2022-05-28 10:36:18 +0100

    :sparkles: feat: add support for local simulator

--- sendchamp.go
+++ sendchamp.go
@@ -3,11 +3,13 @@ package sendchamp
 import "net/http"
 
 const (
-	URLLive = "https://api.sendchamp.com/api/v1"
-	URLTest = "https://sandbox-api.sendchamp.com/api/v1"
+	URLLive           = "https://api.sendchamp.com/api/v1"
+	URLTest           = "https://sandbox-api.sendchamp.com/api/v1"
+	URLLocalSimulator = "http://localhost:2920/api/v1"
 
-	ModeLive = "live"
-	ModeTest = "test"
+	ModeLive           = "live"
+	ModeTest           = "test"
+	ModeLocalSimulator = "local-simulator"
 )
 
 func NewClient(publicKey, mode string) *Client {
@@ -15,6 +17,10 @@ func NewClient(publicKey, mode string) *Client {
 
 	if mode == ModeLive {
 		baseUrl = URLLive
+	}
+
+	if mode == ModeLocalSimulator {
+		baseUrl = URLLocalSimulator
 	}
 
 	// set the base url depending on the mode
